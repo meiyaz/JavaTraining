c96b0247f2e6447613d7432d5d7d1e9332c61a30
Mr-Meiy <kmeiyazhagan14@gmail.com>
2024-04-25 09:49:29 +0530

    added servlet project

---
+++ Servlets/src/main/java/com/training/JavaClass.java
@@ -0,0 +1,37 @@
+package com.training;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JavaClass extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public JavaClass() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Hello World");
+	}
+
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("PUT");
+	}
+
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Deleted");
+	}
+
+}
---
+++ Servlets/src/main/java/com/training/PythonClass.java
@@ -0,0 +1,13 @@
+package com.training;
+
+import java.io.IOException;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class PythonClass extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.getWriter().append("Hello, am from Python class");
+	}
+}
