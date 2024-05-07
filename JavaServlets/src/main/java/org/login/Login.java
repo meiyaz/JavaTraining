103cbc2fb2cfebada98c91e1eb7581cc947b716a
Mr-Meiy <kmeiyazhagan14@gmail.com>
2024-05-07 10:08:33 +0530

    added jwt token

--- JavaServlets/src/main/java/org/login/Login.java
+++ JavaServlets/src/main/java/org/login/Login.java
@@ -1,20 +1,27 @@
 package org.login;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
+import java.util.Base64;
+import java.util.Date;
+import java.util.UUID;
 
-import org.json.JSONArray;
+import java.security.Key;
+
 import org.json.JSONObject;
 import org.utils.CommonUtils;
+
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -22,6 +29,8 @@ public class Login extends HttpServlet {
 	private final String MYSQL_USERNAME = "root";
 	private final String MYSQL_PASSWORD = "root";
 	private String VALIDATE_USER = "SELECT * FROM training.users where username = ? and password = ?;";
+	
+	private final String SECRET_KEY = "SSBhbSB0cnlpbmcgdG8gd3JpdGUgSmF2YSBjb2RlICh0byBiZSBydW4gb24gdGhlIHNlcnZlciBzaWRlKSB0byBnZXQgdGhlIEpXVCB0b2tlbiBwcm9ncmFtYXRpY2FsbHkuIEkgaGF2ZSBhIEpXVCBBcHAgd2l0aCBBUEkgS2V5IGFuZCBzZWNyZXQgZnJvbSB0aGUgc2FtZS4gV2hlbiBydW5uaW5nIHRoZSBjb2RlLCBJIGdldCBhbiBleGNlcHRpb24="; // SECRETKEY123
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
@@ -42,7 +51,11 @@ public class Login extends HttpServlet {
 			ResultSet rs = stmt.executeQuery();
 			if (rs.next()) {
 				responseObj.put("login", "success");
-				responseObj.put("token", "123");
+				// static or random token
+//				responseObj.put("token", "123");
+				
+				// jwt authentication		
+				responseObj.put("token", generateToken(username));			
 			} else
 				responseObj.put("login", "failed");
 			responseObj.put("status", "success");
@@ -53,4 +66,37 @@ public class Login extends HttpServlet {
 		response.getWriter().print(responseObj);
 	}
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().println("<!DOCTYPE html>\r\n"
+				+ "<html lang=\"en\">\r\n"
+				+ "<head>\r\n"
+				+ "    <meta charset=\"UTF-8\">\r\n"
+				+ "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n"
+				+ "    <title>Document</title>\r\n"
+				+ "</head>\r\n"
+				+ "<body>\r\n"
+				+ "    <h2>Hi, myself Mei</h2>\r\n"
+				+ "</body>\r\n"
+				+ "</html>");
+	}
+	
+	
+	private String generateToken(String loginUser) throws Exception {
+		String sessionId = UUID.randomUUID().toString();
+		Date currentDate = new Date();
+		Date expiryDate = new Date(System.currentTimeMillis()+(5*60*1000));
+		Key key = Keys.hmacShaKeyFor(Base64.getDecoder().decode(SECRET_KEY));
+		JwtBuilder builder = Jwts.builder()
+				.id(sessionId)
+				.subject(loginUser)
+				.claim("name", loginUser)
+				.claim("email", "dummymail@gmail.com")
+				.issuedAt(currentDate)
+				.expiration(expiryDate)
+				.signWith(key);
+				//.signWith(key, SignatureAlgorithm.HS256); // define algorithm manually
+		String token = builder.compact();
+		return token;
+	}
+	
 }
