22ce5008bc14bc1357db3f017387086ca20ce8f4
Mr-Meiy <kmeiyazhagan14@gmail.com>
2024-05-30 08:55:36 +0530

    initial commit - SPRING REST (mvn)

---
+++ SpringRestMaven/src/main/java/com/training/SpringRestMaven/SpringRestMavenApplication.java
@@ -0,0 +1,13 @@
+package com.training.SpringRestMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestMavenApplication.class, args);
+	}
+
+}
---
+++ SpringRestMaven/src/test/java/com/training/SpringRestMaven/SpringRestMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.SpringRestMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
